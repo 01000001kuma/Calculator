6523c93272e09e8f04aa9377c73cf96e0d268655
01000001kuma <a_illera@yahoo.com>
2022-10-11 10:18:38 +0200

    handeling 0 clicks

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -30,11 +30,31 @@ const Button = ({value}) => {
         setCalc({ sign: '', num: 0, res: 0 })
     }
 
+    const handleClickButton = () => {
+        const numberString = value.toString()
+        let numberValue;
+        if(numberString === '0' && calc.num === 0) {
+            numberValue = 0
+        } else {
+            numberValue = Number(calc.num + numberString)
+        }
+        
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+
     const handleBtnClick = () => {
 
     const results = {
-        '.': commaClick
-        'C': resetClick,
+        '.': commaClick,
+        'C': resetClick
+    }
+    if(results[value]) {
+        return results[value]()
+    }else{
+        return handleClickButton()
     }
     }
   return (
