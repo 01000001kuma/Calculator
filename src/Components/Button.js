1deff2b502c5e95973a486ddd5ad15b1eef2102a
01000001kuma <a_illera@yahoo.com>
2022-10-11 10:36:01 +0200

    handeling operations

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -45,11 +45,72 @@ const Button = ({value}) => {
         })
     }
 
+    // Click operation
+
+    const signClick = () => {
+        setCalc({
+            sign: value,
+            res: !calc.res && calc.num ? calc.num : calc.res,
+            num: 0
+        })
+    }
+
+    // equals operation
+
+    const equalsClick = () => {
+
+        if(calc.res && calc.num){
+            const math = (a,b, sign) => {
+                const result = {
+                    '+': (a,b) => a + b,
+                    '-': (a,b) => a - b,
+                    'x': (a,b) => a * b,
+                    '/': (a,b) => a / b,
+                } 
+                return result[sign](a,b);
+            }
+            setCalc({
+                res: Math(calc.res,calc.num, calc.sign),
+                sign: '',
+                num: 0
+            })
+
+        }
+    }
+
+    // percent click
+
+    const percentClick = () => {
+        setCalc({
+            num: (calc.num / 100),
+            res: (calc.res / 100),
+            sign: ''
+        })
+    }
+
+    // invert click
+
+    const invertClick = () => {
+        setCalc({
+            num: calc.num ? calc.num * -1 : 0,
+            res: calc.res ? calc.res * -1 : 0,
+            sign: ''
+        })
+    }
+
     const handleBtnClick = () => {
 
     const results = {
         '.': commaClick,
-        'C': resetClick
+        'C': resetClick,
+        '/': signClick,
+        'x': signClick,
+        '-': signClick,
+        '+': signClick,
+        '=': equalsClick,
+        '%': percentClick,
+        '+-': invertClick,
+
     }
     if(results[value]) {
         return results[value]()
